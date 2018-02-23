9f40d15a6a3c8ff5a17cfa8504c922dc6611714d
Min Kim <mk4555@nyu.edu>
2018-02-23 02:05:13 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/ponds/:id/destroy', to: 'ponds#destroy'
   get '/frogs/:id/destroy', to: 'frogs#destroy'
   get '/tadpoles/:id/destroy', to: 'tadpoles#destroy'
-  post '/tadpoles/:id/metamorphose' to: 'tadpoles#'
+  post '/tadpoles/:id/metamorphose'
 
   # code your post to  '/tadpoles/:id/metamorphose' here
   # it should direct to the tadpoles controller, the metamorphose action
@@ -22,5 +22,5 @@ Rails.application.routes.draw do
     resources :tadpoles, :only => [:new]
   end
 
-  
+
 end
